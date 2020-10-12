acf1a9656b6f986f18139cf805df685a28d2f659
A-Salty-Fish <1309032417@qq.com>
2020-10-12 18:56:30 +0800

    餐厅实体

---
+++ Backend/src/main/java/com/example/demo/entity/Canteen.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+public class Canteen {
+    private int id;
+    private String name;
+    private String position;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}

aaeeb248796b1671051fab06482779dda5cc0fe3
A-Salty-Fish <1309032417@qq.com>
2020-10-13 20:10:42 +0800

    完成用户类增删查改

--- Backend/src/main/java/com/example/demo/controller/UserController.java
+++ Backend/src/main/java/com/example/demo/controller/UserController.java
@@ -20,15 +20,71 @@ import java.util.List;
 import java.util.Map;
 @RestController
 public class UserController {
-
+    
+    static String resource = "mybatis-config.xml";
+    static InputStream inputStream;
+    static {
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    static SqlSession session = sqlSessionFactory.openSession();
+    User ReqToUser(HttpServletRequest req){
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
+        String headImgUrl = req.getParameter("headImgUrl");
+        String faculty = req.getParameter("faculty");
+        String schoolNum = req.getParameter("schoolNum");
+        int authorLevel = Integer.parseInt(req.getParameter("authorLevel"));
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setHeadImgUrl(headImgUrl);
+        user.setFaculty(faculty);
+        user.setSchoolNum(schoolNum);
+        user.setAuthorLevel(authorLevel);
+        return user;
+    }
     @RequestMapping("/test")
     public User testMysql() throws IOException {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession session = sqlSessionFactory.openSession();
         List<User> listUser = session.selectList("listUser");
-        return listUser.get(0);
+        return listUser.get(1);
     }
-
+    @GetMapping(value = "/GetUserById/{id}")
+    @ResponseBody
+    public User GetUserById(@PathVariable("id") int id) {
+        User user = session.selectOne("getUser",id);
+        return user;
+    }
+    @GetMapping(value = "/DeleteUserById/{id}")
+    @ResponseBody
+    public User DeleteUserById(@PathVariable("id") int id) {
+        User user = session.selectOne("getUser",id);
+        if (user != null)
+        {
+            session.delete("deleteUser",id);
+            session.commit();
+            return user;
+        }
+        return null;
+    }
+    @PostMapping(value = "/AddUser")
+    @ResponseBody
+    public User AddUser(HttpServletRequest req){
+        User user = ReqToUser(req);
+        session.insert("addUser",user);
+        session.commit();
+        return user;
+    }
+    @PostMapping(value = "/UpdateUser")
+    @ResponseBody
+    public User UpdateUser(HttpServletRequest req){
+        User user = ReqToUser(req);
+        session.update("updateUser",user);
+        session.commit();
+        return user;
+    }
 }

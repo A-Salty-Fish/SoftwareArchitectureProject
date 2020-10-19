4bbcec8a8d671e6f534189e6d46a2cd293c9baff
A-Salty-Fish <1309032417@qq.com>
2020-10-19 09:43:13 +0800

    将撤销功能添加入controller

--- Backend/src/main/java/com/example/demo/controller/UserController.java
+++ Backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,7 @@
 package com.example.demo.controller;
+import com.example.demo.memory.CareTaker;
+import com.example.demo.memory.Originator;
+import com.example.demo.memory.SqlStep;
 import com.example.demo.other.SubPage;
 import com.example.demo.other.SqlSessionFactoryUtil;
 import com.example.demo.entity.User;
@@ -13,8 +16,11 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
-
+    //单例数据库连接
     static SqlSession session = SqlSessionFactoryUtil.openSqlSession();
+    //备忘录模式
+    static Originator originator = new Originator();
+    static CareTaker<User> careTaker = new CareTaker<User>();
 
     @GetMapping(value = "/GetUserById/{id}")
     @ResponseBody
@@ -29,6 +35,11 @@ public class UserController {
         User user = session.selectOne("getUser",id);
         if (user != null)
         {
+            //将删除操作保存进备忘录
+            originator.setState(new SqlStep("delete",id));
+            careTaker.addState(originator.saveStateToMemento());
+            careTaker.addData(user);
+            //数据库删除操作
             session.delete("deleteUser",id);
             session.commit();
             return user;
@@ -36,12 +47,42 @@ public class UserController {
         return null;
     }
 
+    @GetMapping(value = "Redo")
+    @ResponseBody
+    public String RedoUser(){
+        //返回备忘录当前状态
+        SqlStep lastStpe = careTaker.getState().getState();
+        switch (lastStpe.getCmd())//根据上一步操作撤销
+        {
+            case "none":
+                return "nothing to redo";
+            case "add":
+                session.delete("deleteUser",lastStpe.getId());
+                session.commit();
+                return "delete " + lastStpe.getId();
+            case "delete":
+                User user = careTaker.getData();
+                session.insert("addUser",user);
+                session.commit();
+                return "add " + user.getName();
+            default:
+                return "error";
+        }
+    }
+
     @PostMapping(value = "/AddUser")
     @ResponseBody
     public User AddUser(HttpServletRequest req){
+        //根据post请求体创建成员
         User user = User.ReqToUser(req);
+        //数据库添加操作
         session.insert("addUser",user);
         session.commit();
+        //搜索最近新添加的成员id
+        int id = session.selectOne("getLastId");
+        //将添加操作添加进备忘录
+        originator.setState(new SqlStep("add",id));
+        careTaker.addState(originator.saveStateToMemento());
         return user;
     }
 

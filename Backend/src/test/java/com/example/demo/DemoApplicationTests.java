f19455e804c8c1d7897e0d051c20c951f72df85f
A-Salty-Fish <1309032417@qq.com>
2020-10-19 19:23:40 +0800

    尝试测试Bean

--- Backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,45 @@
 package com.example.demo;
 
+import com.example.demo.entity.User;
+import com.example.demo.memory.CareTaker;
+import com.example.demo.memory.Originator;
+import com.example.demo.memory.SqlStep;
+import com.example.demo.other.SqlSessionFactoryUtil;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        SqlSession session = SqlSessionFactoryUtil.openSqlSession();
+        int lastId = session.selectOne("getLastId");
+        System.out.println(lastId);
     }
 
+    @Test
+    void testMem(){
+        Originator originator = new Originator();
+        CareTaker careTaker = new CareTaker();
+        originator.setState(new SqlStep("add ",13));
+        careTaker.addState(originator.saveStateToMemento());
+        originator.setState(new SqlStep("delete ",13));
+        careTaker.addState(originator.saveStateToMemento());
+        SqlStep current = careTaker.getState().getState();
+        SqlStep last = careTaker.getState().getState();
+        System.out.println(current.getCmd()+current.getId());
+        System.out.println(last.getCmd()+last.getId());
+    }
+
+    @Test
+    void testSpring(){
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        User user = (User)context.getBean("user");
+        System.out.println(user.getFaculty());
+    }
 }

0d66b61705ff1be79f1ca6f8915965e2afe1c316
A-Salty-Fish <1309032417@qq.com>
2020-10-25 16:08:06 +0800

    sjy的sql脚本

--- SqlScript/comment.sql
+++ SqlScript/comment.sql
@@ -12,3 +12,85 @@ create table comment
 
 INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (1, '鸡排饭好评', 1, '鸡排饭', '信息学部三食堂', 5);
 INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (2, '牛肉面一般', 1, '牛肉面', '信息学部三食堂', 3);
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (3,'重庆辣子鸡
+一般',1,'重庆辣子鸡
+','信息学部三食堂',3);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (4,'红烧排骨 
+一般',1,'红烧排骨 
+','信息学部三食堂',3);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (5,'香辣鸡柳 
+好评',1,'香辣鸡柳 
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (6,'粉蒸肉 
+好评',1,'粉蒸肉 
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (7,'爆炒香肠 
+好评',1,'爆炒香肠 
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (8,'清炒肉丝 
+好评',1,'清炒肉丝 
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (9,'牛肉丸子 
+一般',1,'牛肉丸子 
+','信息学部三食堂',3);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (10,'原味鸡腿 
+好评',1,'原味鸡腿 
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (11,'糖醋里脊
+一般',1,'糖醋里脊
+','信息学部三食堂',3);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (12,'红烧鸡块
+一般',1,'红烧鸡块
+','信息学部三食堂',3);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (13,'农家红烧肉
+好评',1,'农家红烧肉
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (14,'干烧糍粑鱼
+好评',1,'干烧糍粑鱼
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (15,'手工肉丸汤E
+好评',1,'手工肉丸汤E
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (16,'黄瓜肉片
+一般',1,'黄瓜肉片
+','信息学部三食堂',3);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (17,'香味卤鸭
+好评',1,'香味卤鸭
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (18,'红烧鲫鱼
+好评',1,'红烧鲫鱼
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (19,'蒙古牛柳
+一般',1,'蒙古牛柳
+','信息学部三食堂',3);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (20,'红烧鸡丁
+一般',1,'红烧鸡丁
+','信息学部三食堂',3);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (21,'湘味牛肉
+好评',1,'湘味牛肉
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (22,'香菇鸡块
+好评',1,'香菇鸡块
+','信息学部三食堂',5);
+
+INSERT INTO `comment`(`id`, `content`, `user_id`, `food_name`, `canteen`, `stars`) VALUES (23,'辣子鸡丁好评',1,'辣子鸡丁','信息学部三食堂',5);
+
--- SqlScript/food.sql
+++ SqlScript/food.sql
@@ -12,3 +12,65 @@ INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (1, '鸡排饭', N
 INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (2, '鸡腿饭', NULL, '信息学部三食堂');
 INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (3, '烤肉拌饭', NULL, '信息学部三食堂');
 INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (4, '牛肉面', NULL, '信息学部三食堂');
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (5,'重庆辣子鸡
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (6,'红烧排骨 
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (7,'香辣鸡柳 
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (8,'粉蒸肉 
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (9,'爆炒香肠 
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (10,'清炒肉丝 
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (11,'牛肉丸子 
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (12,'原味鸡腿 
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (13,'糖醋里脊
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (14,'红烧鸡块
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (15,'农家红烧肉
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (16,'干烧糍粑鱼
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (17,'手工肉丸汤E
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (18,'黄瓜肉片
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (19,'香味卤鸭
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (20,'红烧鲫鱼
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (21,'蒙古牛柳
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (22,'红烧鸡丁
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (23,'湘味牛肉
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (24,'香菇鸡块
+',NULL,'信息学部三食堂');
+
+INSERT INTO `food`(`id`, `name`, `img_url`, `canteen`) VALUES (25,'辣子鸡丁',NULL,'信息学部三食堂');
+
